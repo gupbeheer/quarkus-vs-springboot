5dff9c8ff77798b4af6e1bb0e1319ff5986b9bb0
MilovdZee <milo@vanderzee.org>
2022-05-27 16:56:22 +0200

    added k8s yamls

--- scripts/actions.sh
+++ scripts/actions.sh
@@ -8,16 +8,34 @@ cd $VERSION || exit
 
 ACTION=$2
 case $ACTION in
+  start-minikube)
+    minikube start --nodes 3
+    minikube addons enable ingress
+    ;;
+  stop-minikube)
+    minikube stop
+    ;;
+  delete-minikube)
+    minikube delete
+    ;;
   compile-controller)
     cd order-controller || exit
     gradle build
     ;;
   build-controller-image)
     cd order-controller || exit
-    docker build . --tag $VERSION/order-controller:latest
+    gradle build
+    minikube image build --all --tag spring-boot/order-controller:latest .
     ;;
-  run-controller-image)
-    docker run $VERSION/order-controller:latest
+  start-controller-pods)
+    cd order-controller || exit
+    kubectl apply -f kubernetes.yml
+    ;;
+  start-ingress)
+    cd $DIRNAME/.. || exit
+    kubectl apply -f ingress.yml
+    kubectl get ingress
+    echo "Connect your browser to the shown ip address and port number"
     ;;
   *)
     echo "Usage:"
@@ -28,7 +46,11 @@ case $ACTION in
     echo "  - quarkus"
     echo
     echo "  action:"
-    echo "  - compile-controller        Compile the controller-service"
-    echo "  - build-controller-image    Build the order-service docker image"
-    echo "  - run-controller-image      Start the controller-service in docker"
+    echo "  - start-minikube            Start a multi-node kubernetes cluster"
+    echo "  - stop-minikube             Stop the minikube cluster"
+    echo "  - delete-minikube           Delete the minikube cluster"
+    echo "  - compile-controller        Compile the order-controller"
+    echo "  - build-controller-image    Build the order-controller docker image"
+    echo "  - start-controller-pods     Start the order-controller in kubernetes"
+    echo "  - start-ingress             Start kubernetes Ingress"
 esac
--- spring-boot/order-controller/src/test/kotlin/org/demo/order-controller/LoadContextTests.kt
+++ spring-boot/order-controller/src/test/kotlin/org/demo/order-controller/LoadContextTests.kt
@@ -1,13 +1,12 @@
-package org.demo.controller
+package org.demo.`order-controller`
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class OrderControllerApplicationTests {
+class LoadContextTests {
 
 	@Test
 	fun contextLoads() {
 	}
-
 }
